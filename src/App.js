cca88ad306410dbde3eab4f586d3f3b328c685d5
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-12-26 23:41:39 +0600

    NavBar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
+import { AppBar, CssBaseline, Toolbar, Typography } from '@mui/material';
+import { PhotoCamera } from '@mui/icons-material';
 
 function App() {
   return (
-    <div className="">
-      <h2>hello Rocky</h2>
+    <div>
+      <CssBaseline>
+        <AppBar position='relative'>
+          <Toolbar>
+            <PhotoCamera></PhotoCamera>
+            <Typography variant='h5'>
+                Photo Gallery
+            </Typography>
+          </Toolbar>
+
+        </AppBar>
+      </CssBaseline>
+      <main>
+        
+      </main>
+      
     </div>
   );
 }
